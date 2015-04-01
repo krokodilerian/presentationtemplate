ced30eb33cbc7d6f112250ad2ec9850c4aefa676
Vasil Kolev <vasil@ludost.net>
2015-04-01 23:36:25 +0300

    fix for named pics

--- makedep.sh
+++ makedep.sh
@@ -2,5 +2,5 @@
 
 for i in *.pandoc; do
 	pdfn=`echo $i|sed s/\.pandoc$/.pdf/`
-	echo $pdfn: `grep '^!' "$i"|cut -d \( -f 2|cut -d \) -f 1|sort|uniq `
+	echo $pdfn: `grep '^!' "$i"|cut -d \( -f 2|cut -d \) -f 1|cut -d ' ' -f 1|sort|uniq `
 done
